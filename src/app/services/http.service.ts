8dee78de7ed6db82e963b604013c87cb4a3eeed1
urigross <urigross@gmail.com>
2022-04-14 16:07:05 +0300

    Added error handling to http service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Answer } from '../models/answer.model';
-import { take } from 'rxjs';
+import { take, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +27,9 @@ export class HttpService {
   async getDatabaseId(): Promise<boolean>{
     // TODO: Add error catching
     const ANSWERS_URL = "http://localhost:3000/bugReportForm";
-    const ans = await this.httpClient.get(ANSWERS_URL);
+    const ans = await this.httpClient.get(ANSWERS_URL).pipe(
+      catchError(this.handleError)
+    )
     return new Promise(resolve => {
       ans.pipe(
         take(1)
@@ -40,6 +42,19 @@ export class HttpService {
     })
   }
 
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
+  }
+
   private async _save(objToJSON: Object) {
     const ANSWERS_URL = "http://localhost:3000/bugReportForm";
     this.httpClient.post<any>(ANSWERS_URL, objToJSON).subscribe(data => {
@@ -48,9 +63,23 @@ export class HttpService {
 
   private async _update(answers: Answer[]) {
     const POST_ANSWERS_URL = "http://localhost:3000/bugReportForm/1";
-    this.httpClient.put<any>(POST_ANSWERS_URL, answers).subscribe(data => { })
+    this.httpClient.put<any>(POST_ANSWERS_URL, answers).subscribe(
+      val => {
+        console.log("PUT call successful value returned in body", 
+                    val);
+    },
+    response => {
+        console.log("PUT call in error", response);
+    },
+    () => {
+        console.log("The PUT observable is now completed.");
+    }
+    )
   }
 }
+     
+  
+
 
 
 
