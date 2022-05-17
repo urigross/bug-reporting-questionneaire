b13ede20083ba425b284b56297a41d7dcebc7a5b
urigross <urigross@gmail.com>
2022-05-17 17:13:44 +0300

    updated answers to be written into JSON API, Removed code from http service

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,31 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Answer } from '../models/answer.model';
-import { take, catchError, throwError } from 'rxjs';
+import { take, catchError, of, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  async saveAnswers(answers: Answer[]) {
-    const objToJSON: object = { "id": 1, 'answers': answers };
-    // If DB id exits - update else save DB to JSON.
-    let isUpdate:boolean = await this.getDatabaseId();
-    if (isUpdate) {
-      this._update(answers);
-    }
-    else {
-      this._save(objToJSON);
-    }
-  }
-// Check is DB exists by fetching it's id.
-  async getDatabaseId(): Promise<boolean>{
-    const ANSWERS_URL = "http://localhost:3000/bugReportForm";
-    const ans = await this.httpClient.get(ANSWERS_URL).pipe(
-      catchError(this.handleError)
+  // Check is DB exists by fetching it's id.
+  async getDatabaseId(): Promise<boolean> {
+    const ANSWERS_URL = "https://627695de15458100a6b0b7e5.mockapi.io/bugReport/answers";
+    const ans = await of(this.http.get(ANSWERS_URL)).pipe(
+      // catchError(this.handleError)
+      catchError(this._handleError<boolean>('getDatabaseId'))
+
     )
     return new Promise(resolve => {
       ans.pipe(
@@ -39,43 +33,46 @@ export class HttpService {
     })
   }
 
-  handleError(error: HttpErrorResponse) {
-    let errorMessage = 'Unknown error!';
-    if (error.error instanceof ErrorEvent) {
-      // Client-side errors
-      errorMessage = `Error: ${error.error.message}`;
-    } else {
-      // Server-side errors
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+
+
+  private _handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
     }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
   }
 
-  private async _save(objToJSON: Object) {
-    const ANSWERS_URL = "http://localhost:3000/bugReportForm";
-    this.httpClient.post<any>(ANSWERS_URL, objToJSON).subscribe(data => {
-    })
+
+  saveAnswers(answers: Answer[]): Observable<object> {
+    const ANSWERS_URL = "https://627695de15458100a6b0b7e5.mockapi.io/bugReport/answers";
+    return of(this.http.post<Object>(ANSWERS_URL, answers, this.httpOptions).subscribe(
+      {
+        next: (v) => console.log(v),
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+      }
+    )
+    )
   }
 
   private async _update(answers: Answer[]) {
-    const POST_ANSWERS_URL = "http://localhost:3000/bugReportForm/1";
-    this.httpClient.put<any>(POST_ANSWERS_URL, answers).subscribe(
-      val => {
-        console.log("PUT call successful value returned in body", 
-                    val);
-    },
-    response => {
-        console.log("PUT call in error", response);
-    },
-    () => {
-        console.log("The PUT observable is now completed.");
-    }
-    )
+    const POST_ANSWERS_URL = "https://627695de15458100a6b0b7e5.mockapi.io/bugReport/answers";
+    of(this.http.put<any>(POST_ANSWERS_URL, answers)).subscribe({
+      next: (v) => console.log(v),
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    })
   }
 }
-     
-  
+
+
 
 
 
